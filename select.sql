77fe4e8cd27295fd01fb80b9b51442e11f84e789
Urazov Dmitriy <urazov.d@gmail.com>
2016-02-02 23:59:16 +0300

    format query

--- select.sql
+++ select.sql
@@ -1,8 +1,8 @@
 select i.type, i.value as v
 from info as i, (
-				select max(i2.date) as dt, i2.type
-				from info i2
-				group by type
-				) as s2
+  select max(i2.date) as dt, i2.type
+  from info i2
+  group by type
+  ) as s2
 where i.date = s2.dt
 and i.type = s2.type;
